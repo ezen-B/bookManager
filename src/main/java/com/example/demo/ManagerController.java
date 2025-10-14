ece420a98790aa05a918681e76fa9464f740044b
hs <ina214@naver.com>
2025-10-14 14:10:33 +0900

    [1014] 기본 레이아웃

--- src/main/java/com/example/demo/ManagerController.java
+++ src/main/java/com/example/demo/ManagerController.java
@@ -2,10 +2,23 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class ManagerController {
 	@Autowired
 	private ManagerService service;
 
+  @GetMapping("/")
+  public String home() {
+      return "redirect:/main";
+  }
+
+  @GetMapping("/main")
+  public String main() {
+      return "/main";
+  }
+  
+  
 }
