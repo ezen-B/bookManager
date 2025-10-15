6eea954506c76b958285e4f5d2dd91986761d0e6
hs <ina214@naver.com>
2025-10-15 11:18:12 +0900

    [1015] 로그인/로그아웃 추가

--- src/main/java/com/example/demo/ManagerController.java
+++ src/main/java/com/example/demo/ManagerController.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -20,5 +23,16 @@ public class ManagerController {
       return "/main";
   }
   
-  
+  @PostMapping("/loginOk")
+  public String loginOk(AdminMemberDto mdto,
+      HttpSession session)
+  {
+    return service.loginOk(mdto,session);
+  }
+
+  @GetMapping("/logout")
+  public String logout(HttpSession session)
+  {
+    return service.logout(session);
+  }
 }
--- src/main/java/com/example/demo/ManagerService.java
+++ src/main/java/com/example/demo/ManagerService.java
@@ -3,9 +3,34 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpSession;
+
 @Service
 public class ManagerService {
 	@Autowired
 	private ManagerMapper mapper;
 
+  public String loginOk(AdminMemberDto mdto, HttpSession session) 
+  {
+    // 실행전 : 아이디 , 비밀번호
+
+    mdto=mapper.loginOk(mdto);
+    
+    // 실행후 : 아이디 , 이름 , 레벨
+    
+    if(mdto != null)// 회원이 맞으면 => 세션변수 : 아이디 , 이름 , 레벨
+    {
+      session.setAttribute("userid", mdto.getUserid());
+      session.setAttribute("name", mdto.getName());
+      session.setAttribute("level", mdto.getLevel());
+    }
+      
+    return "redirect:/main";
+  }
+
+  public String logout(HttpSession session)
+  {
+    session.invalidate();
+    return "redirect:/main";
+  }
 }
